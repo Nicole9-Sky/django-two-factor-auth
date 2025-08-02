dc48b97c1b1ba02394958b549f9328e1aee7baa9
Nicole9-Sky <cezeh247@gmail.com>
2025-08-02 02:23:46 +0100

    Remove auto superuser creation

--- example/wsgi.py
+++ example/wsgi.py
@@ -3,11 +3,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'example.settings')
 
-try:
-    from .create_superuser import run
-    run()
-except Exception as e:
-    print(f"Error creating superuser: {e}")
-
-
 application = get_wsgi_application()
